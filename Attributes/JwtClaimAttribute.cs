09afa5a0eb58e9a17c88ee98526fdc0a17417e78
Jean LAURENT <jeanz.laurent@gmail.com>
2025-12-04 21:01:17 +0100

    Refactor JwtClaimAttribute to initialize ClaimType directly

--- Attributes/JwtClaimAttribute.cs
+++ Attributes/JwtClaimAttribute.cs
@@ -1,12 +1,7 @@
 namespace MiniJwt.Core.Attributes;
 
 [AttributeUsage(AttributeTargets.Property)]
-public class JwtClaimAttribute : Attribute
+public class JwtClaimAttribute(string claimType) : Attribute
 {
-    public string ClaimType { get; }
-
-    public JwtClaimAttribute(string claimType)
-    {
-        ClaimType = claimType;
-    }
+    public string ClaimType { get; } = claimType;
 }
