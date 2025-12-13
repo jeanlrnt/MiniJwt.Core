386b853bc6ca1508937f7b77696d657289e6a2dd
Jean LAURENT <jeanz.laurent@gmail.com>
2025-12-13 19:21:42 +0100

    refactor(jwt): Change token generation method to support nullable return type - IMiniJwtService.cs

--- src/MiniJwt.Core/Services/IMiniJwtService.cs
+++ src/MiniJwt.Core/Services/IMiniJwtService.cs
@@ -4,7 +4,7 @@ namespace MiniJwt.Core.Services;
 
 public interface IMiniJwtService
 {
-    string GenerateToken<T>(T payload);
+     string? GenerateToken<T>(T payload);
     ClaimsPrincipal? ValidateToken(string token);
     T? ValidateAndDeserialize<T>(string token) where T : new();
 }
