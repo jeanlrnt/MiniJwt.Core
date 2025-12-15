df4dc2106ea3c3debd5ab2d20a0d5b57b8678030
Jean LAURENT <jeanz.laurent@gmail.com>
2025-12-15 18:53:55 +0100

    test(validation): Add validation test for invalid expiration in MiniJwtOptions - MiniJwtTests.Validation.cs

--- src/MiniJwt.Tests/MiniJwtTests.Validation.cs
+++ src/MiniJwt.Tests/MiniJwtTests.Validation.cs
@@ -66,4 +66,24 @@ public partial class MiniJwtTests
             await host.StartAsync();
         });
     }
+    
+    [Fact]
+    public async Task ValidateOnStart_WithInvalidExpiration_ShouldThrowOptionsValidationException()
+    {
+        var builder = Host.CreateDefaultBuilder()
+            .ConfigureServices((_, services) =>
+            {
+                services.AddMiniJwt(opts =>
+                {
+                    opts.SecretKey = ValidSecretKey;
+                    opts.ExpirationMinutes = 0;
+                });
+            });
+
+        await Assert.ThrowsAsync<OptionsValidationException>(async () =>
+        {
+            using var host = builder.Build();
+            await host.StartAsync();
+        });
+    }
 }
