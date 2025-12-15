338125149c83bac8755e70d214ead3f9e3313db3
Jean LAURENT <jeanz.laurent@gmail.com>
2025-12-15 17:57:41 +0100

    feat(extensions): Add extension method for MiniJwt service registration - ServiceCollectionExtensions.cs

---
+++ src/MiniJwt.Core/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using MiniJwt.Core.Models;
+using MiniJwt.Core.Services;
+
+namespace MiniJwt.Core.Extensions;
+
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddMiniJwt(this IServiceCollection services, Action<MiniJwtOptions> configure)
+    {
+        services.Configure(configure);
+        services.AddSingleton<IMiniJwtService, MiniJwtService>();
+
+        return services;
+    }
+}
