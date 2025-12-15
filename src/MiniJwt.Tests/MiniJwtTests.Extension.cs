09d8d54280ea317c40cc4122b050cadabb5c2d35
Jean LAURENT <jeanz.laurent@gmail.com>
2025-12-15 17:58:39 +0100

    test(tests): Add partial class for MiniJwtTests - MiniJwtTests.Extension.cs

---
+++ src/MiniJwt.Tests/MiniJwtTests.Extension.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using MiniJwt.Core.Attributes;
+using MiniJwt.Core.Models;
+using MiniJwt.Core.Services;
+using Xunit;
+
+namespace MiniJwt.Tests;
+
+public partial class MiniJwtTests
+{
+    
+}
