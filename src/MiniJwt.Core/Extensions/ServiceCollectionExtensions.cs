16ee513b7740e7adde67e2061f77d72a78e27a93
Jean LAURENT <jeanz.laurent@gmail.com>
2025-12-15 18:36:50 +0100

    feat(extensions): Register MiniJwtOptionsValidator for options validation - ServiceCollectionExtensions.cs

--- src/MiniJwt.Core/Extensions/ServiceCollectionExtensions.cs
+++ src/MiniJwt.Core/Extensions/ServiceCollectionExtensions.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿ using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 using MiniJwt.Core.Models;
 using MiniJwt.Core.Services;
+using MiniJwt.Core.Validators;
 
 namespace MiniJwt.Core.Extensions;
 
@@ -14,6 +16,7 @@ public static class ServiceCollectionExtensions
 
         var builder = services.AddOptions<MiniJwtOptions>().Configure(configure);
 
+        builder.Services.AddSingleton<IValidateOptions<MiniJwtOptions>, MiniJwtOptionsValidator>();
         builder.ValidateOnStart();
 
         services.AddLogging();
